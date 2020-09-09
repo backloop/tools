3c96939e229f7f8e5c3d686dd0f59b04b71137b1
Pablo Cases <cases.pablo@gmail.com>
2020-09-09 14:51:28 +0200

    vim: Make GVim colorschemes eork in console vim
    This was needed to make "rainbow" coloring of commit hashes in
    vim-fugitive's :Gblame command

--- vim/install.sh
+++ vim/install.sh
@@ -82,6 +82,9 @@ package https://github.com/ntpeters/vim-better-whitespace.git &
 package https://github.com/ctrlpvim/ctrlp.vim &
 package https://github.com/tpope/vim-obsession &
 package https://github.com/tpope/vim-fugitive &
+# CSApprox: make GVim colorschemes work in terminal mode
+# CSApprox: also make git commit hash coloring in fugitive's Gblame work
+package https://github.com/godlygeek/csapprox &
 package https://github.com/vim-airline/vim-airline &
 package https://github.com/jeffkreeftmeijer/vim-numbertoggle &
 archive http://tamacom.com/global/global-6.6.4.tar.gz gtags.vim &
